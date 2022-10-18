1446f325179ae37f4b6b9075098da1ba652dd64b
Harshit Mody <55501384+harshitmody72@users.noreply.github.com>
2022-10-19 00:46:51 +0530

    Add files via upload

---
+++ Java/Hello World/TryCatch.java
@@ -0,0 +1,12 @@
+public class TryCatch {
+    public static void main(String[] args) {
+//        try{
+//            System.out.println("hiii");
+//            int b = 0;
+//            int a = 10/b;
+//        }catch (ArithmeticException e){
+//            System.out.println(e + " go");
+//        }
+
+    }
+}
