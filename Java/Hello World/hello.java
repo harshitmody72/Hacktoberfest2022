ebf851c2f6f599e330cdb090c6403eb01353ff76
mrBanti <116115388+mrBanti@users.noreply.github.com>
2022-10-19 01:15:01 +0530

    Update hello.java

--- Java/Hello World/hello.java
+++ Java/Hello World/hello.java
@@ -4,6 +4,6 @@ import java.sql.SQLOutput;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello");
  }
 }
